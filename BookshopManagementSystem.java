a847874398d3d8697e0a767a44cb997b55cab263
chethanavirajini <cheth.virajini@gmail.com>
2023-10-24 12:57:05 +0530

    pages created

--- BookshopManagementSystem.java
+++ BookshopManagementSystem.java
@@ -45,6 +45,7 @@ public class BookshopManagementSystem{
     }
 
     public static void ShowBookshopMenu(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("+-------------------------------------------------------------------------------+");
         System.out.println("|                 WELCOME TO SHI BOOK SHOP MANAGEMENT SYSTEM                    |");
         System.out.println("+-------------------------------------------------------------------------------+");
@@ -53,7 +54,71 @@ public class BookshopManagementSystem{
         System.out.println("[3]Upadate Book                                             [4]Show All Books");
         System.out.println("[5]Logout                                                   [6]Exit the System");
 
+        System.out.print("ENTER AN OPTION TO CONTINUE: ");
+        String option = sc.next();
 
+        switch(option){
+			case "1":
+				AddBook();
+				break;
+			case "2":
+				DeleteBook();
+				break;
+			case "3":
+				UpdateBook();
+				break;
+            case "4":
+				ShowAll();
+				break;
+            case "5":
+                LogOut();
+                break;
+            case "6":
+                ExitSystem();
+                break;
+            default:
+                ShowBookshopMenu();
+
+		}
+
+        sc.close();
+    }
+
+    public static void AddBook(){
+         System.out.println("+-------------------------------------------------------------------------------+");
+         System.out.println("|                               ADD BOOK                                        |");
+         System.out.println("+-------------------------------------------------------------------------------+");
+    }
+
+    public static void DeleteBook(){
+         System.out.println("+-------------------------------------------------------------------------------+");
+         System.out.println("|                               DELETE BOOK                                     |");
+         System.out.println("+-------------------------------------------------------------------------------+");
+
+    }
+
+    public static void UpdateBook(){
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|                               UPDATE BOOK                                     |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+    }
+
+    public static void ShowAll(){
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|                               SHOW ALL BOOK                                   |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+    }
+
+    public static void LogOut(){
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|                               LOG OUT                                         |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+    }
+
+    public static void ExitSystem(){
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|                              Exit the System                                  |");
+        System.out.println("+-------------------------------------------------------------------------------+");
     }
 
 
