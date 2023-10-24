2a4690bea72b06e24ec7edeb04d9087747eab5ea
chethanavirajini <cheth.virajini@gmail.com>
2023-10-24 12:43:59 +0530

    Login page completed

--- BookshopManagementSystem.java
+++ BookshopManagementSystem.java
@@ -4,32 +4,44 @@ public class BookshopManagementSystem{
     public static void main(String[] args){
         ShowloginPage();
     }
-
     public static void ShowloginPage(){
           Scanner sc = new Scanner(System.in);
         System.out.println("+-------------------------------------------------------------------------------+");
         System.out.println("|                                   LOGIN PAGE                                  |");
         System.out.println("+-------------------------------------------------------------------------------+");
 
-        String dbUsername = "chethana";
+        String dbUsername = "admin";
         String dbPassword = "1234";
 
-        System.out.println("Enter your username: ");
-        String username = sc.next();
-        System.out.println("Enter your password: ");
-        String password = sc.next();
+        boolean loginpage = true;
 
-        sc.close();
-        if(username != dbUsername && password!= dbPassword){
+        while(loginpage){
+            System.out.print("Enter your username: ");
+            String username = sc.next();
+            System.out.print("Enter your password: ");
+            String password = sc.next();
+
+        
+        if(!username.equals(dbUsername) && !password.equals(dbPassword)){
             System.out.println("Wrong username and password..! Try again..");
-        }else if(username != dbUsername && password == dbPassword){
+            loginpage = true;
+            
+        }else if(!username.equals(dbUsername) && password.equals(dbPassword)){
             System.out.println("Wrong username..! Try again..");
-        }else if(username == dbUsername && password != dbPassword){
+            loginpage = true;
+           
+        }else if(username.equals(dbUsername) && !password.equals(dbPassword)){
             System.out.println("Wrong password..! Try again..");
+            loginpage = true;
+            
         }else{
             System.out.println("Login Successful!");
+            loginpage = false;
             ShowBookshopMenu();      
         }
+
+        }  
+        sc.close();
     }
 
     public static void ShowBookshopMenu(){
@@ -37,7 +49,9 @@ public class BookshopManagementSystem{
         System.out.println("|                 WELCOME TO SHI BOOK SHOP MANAGEMENT SYSTEM                    |");
         System.out.println("+-------------------------------------------------------------------------------+");
 
-        
+        System.out.println("[1]Add Book                                                 [2]Delete Book");
+        System.out.println("[3]Upadate Book                                             [4]Show All Books");
+        System.out.println("[5]Logout                                                   [6]Exit the System");
 
 
     }
